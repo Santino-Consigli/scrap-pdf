29359b6c323f2f4c05ea65dc1391b7778edef6de
unknown <Salud@INV-36029.cordoba.local>
2025-02-11 13:54:04 -0300

    progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import fitz
 import os
+import re
 from medicamento import *
 from encabezado import * 
 #leer pdf de la carpeta input
@@ -16,46 +17,39 @@ texto = pag.get_text().split('\n')
 
 
 def getData():
-    # print(len(texto))
-    # print("probando")
-    # print(texto[0][22:])
-    # print(texto[1][7:])
-    # print(texto[2][19:])
-    # print(texto[3][21:])
-    # print(texto[4][17:-1])
-    # print(texto[5][26:])
-    # print(texto[6][11:])
-    # print(texto[7][20:])
-
-    # contador = 9
-    # for i in range(len(texto)):
-    #     if i > 29:
-    #         swithc (contador >= 9):
-    #             print("nombremedicamento" + texto[i])
-    #         if contador = 8
-    #             contador -= 1
-            
-
-
-    #cargar en la clase encabezado
-
+    word = "Medicamentos"
+    textoEncabezado = ''
+    textoMedicamentos = ''
+    indexToMedicamentos =0
+    for index, linea in enumerate(texto):
+        if word in linea:
+            indexToMedicamentos = index
+            print(linea)
+            break
+        else:
+            textoEncabezado += linea + '\n'
+            print(textoEncabezado)
+    #todoo get data from header with regexs and create instance of Encabezado
     nuevoEncab = Encabezado(texto[0][22:], texto[1][7:],texto[2][19:],texto[3][21:], texto[4][17:-1], texto[5][26:], texto[6][11:], texto[7][20:])
-    # nuevoEncab.numForm = texto[0][22:]
-    # nuevoEncab.fecha = texto[1][7:]
-    # nuevoEncab.respCarga = texto[2][19:]
-    # nuevoEncab.numDoc = texto[3][21:]
-    # nuevoEncab.establecimiento = texto[4][17:-1]
-    # nuevoEncab.fechaFormu = texto[5][26:]
-    # nuevoEncab.consultas = texto[6][11:]
-    # nuevoEncab.recetas = texto[7][20:]
     print(nuevoEncab.toString())
 
-    #crgar medicamentos, recorro con ciclo ford
-    
-    
+    for index in range(indexToMedicamentos+1, len(texto)):
+        textoMedicamentos += texto[index] + '\n'
+        #print(textoMedicamentos)
+    regexCodeMedication = r'^\d{3} '
+    contador = 0
+    for linea in textoMedicamentos.split('\n'):
+        if re.search(regexCodeMedication, linea):
+            
+            contador += 1
+            print(linea)
+            if(contador == 6):
+                break
+
 
 if __name__ == "__main__":
     os.system('cls')
-    print(texto)
-    print("- - - - Obtener data - - - - ")
+    getData()
+    # print(texto)
+    # print("- - - - Obtener data - - - - ")
     # getData()
