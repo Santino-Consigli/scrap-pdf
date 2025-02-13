4f785047d10ca9b3edfbf1a21cf73dc90f6e108a
unknown <Salud@INV-36010.cordoba.local>
2025-02-13 08:25:56 -0300

    +atributo de clase medicamento

--- medicamento.py
+++ medicamento.py
@@ -1,11 +1,12 @@
 class Medicamento:
  
-    def __init__(self, descripcion, stock_inicial, unidades_CSL, unidades_recibidas, UnEnt, otras_salidas_NoApto, otras_salidas, ajuste, stock_final):
+    def __init__(self, descripcion, stock_inicial, unidades_CSL, unidades_recibidas_clering, UnEnt, otras_salidas_clering, otras_salidas_NoApto, otras_salidas, ajuste, stock_final):
         self.descripcion = descripcion
         self.stock_inicial = stock_inicial
         self.unidades_CSL = unidades_CSL
-        self.unidades_recibidas = unidades_recibidas
+        self.unidades_recibidas_clering = unidades_recibidas_clering
         self.UnEnt = UnEnt
+        self.otras_salidas_clering = otras_salidas_clering
         self.otras_salidas_NoApto = otras_salidas_NoApto
         self.otras_salidas = otras_salidas
         self.ajuste = ajuste
